ace149ca6271b5454ab0930f43d628a94b01b431
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-06-15 15:41:40 -0300

    Added a dedicated TOML configuration to run Cosmic Ray for local distributor

--- mutation_testing/launch-local-distributor.sh
+++ mutation_testing/launch-local-distributor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # From pycalphad root's folder, run this command:
-uv run cosmic-ray exec mutation_testing/cosmic-ray-base-config.toml mutation_testing/cosmic-ray-for-local-distributor.sqlite
+uv run cosmic-ray exec mutation_testing/cosmic-ray-local-distributor.toml mutation_testing/cosmic-ray-for-local-distributor.sqlite
