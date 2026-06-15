ba5626d63ab7ccc833cfbefbd0ec8d3f16abe76a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-06-15 15:28:17 -0300

    Script to launch local distributor

---
+++ mutation_testing/launch-local-distributor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# From pycalphad root's folder, run this command:
+uv run cosmic-ray exec mutation_testing/cosmic-ray-base-config.toml mutation_testing/cosmic-ray-for-local-distributor.sqlite
