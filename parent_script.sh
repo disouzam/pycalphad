c742fba32d3ed60c113dac0ead1ba1daf6d9d101
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-12 18:38:45 -0300

    Sample scripts to test delegated execution and killing process

---
+++ parent_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bash child_script_1.sh &
+pid1=$!
+echo "Child script 1 started with PID $pid1"
+
+bash child_script_2.sh &
+pid2=$!
+echo "Child script 2 started with PID $pid2"
+
+sleep 10
+
+echo "Stopping child script 1..."
+kill $pid1
+
+sleep 10
+
+echo "Stopping child script 2..."
+kill $pid2
