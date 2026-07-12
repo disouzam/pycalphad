126790926b242d3c99a7fa09e814598f357c9459
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-12 15:01:02 -0300

    Working version of launcher with issues with repeated launching of Cosmic Ray solved

--- mutation_testing/launch_local_distributor_and_monitor.sh
+++ mutation_testing/launch_local_distributor_and_monitor.sh
@@ -4,14 +4,21 @@ cleanup() {
     local distributor_pid="$1"
     local monitor_pid="$2"
     kill "${distributor_pid}" "${monitor_pid}" 2>/dev/null
+    exit 1
 }
 
+distributor_pid=""
 while true; do
-    bash mutation_testing/Local_distributor/launch.sh &
-    distributor_pid=$!
-    echo "Local distributor PID: ${distributor_pid}"
 
-    bash mutation_testing/check_status_periodically.sh "$1" &
+    if [[ -n "${distributor_pid}" ]]; then
+        echo "Local distributor is already running with PID: ${distributor_pid}."
+    else
+        source mutation_testing/Local_distributor/launch.sh &
+        distributor_pid=$!
+        echo "Local distributor PID: ${distributor_pid}"
+    fi
+
+    source mutation_testing/check_status_periodically.sh "$1" --single-check &
     monitor_pid=$!
     echo "Monitor PID: ${monitor_pid}"
 
@@ -22,7 +29,22 @@ while true; do
     echo "Monitor return code: ${monitor_rc}"
 
     if [ "${monitor_rc}" -eq 1 ]; then
+        echo -e "\n#######################################################################"
+        echo "Monitor detected no progress. Stopping the local distributor."
+        echo -e "\n#######################################################################"
         kill "${distributor_pid}" 2>/dev/null
+        distributor_pid=""
+        trap - INT TERM EXIT
+        printf '\a'
+        git restore "*.py"
+        python_files_changed=$(git st | grep -E 'modified:.*\.py' | wc -l)
+
+        if [[ "${python_files_changed}" -gt 0 ]]; then
+            echo -e "\n#######################################################################"
+            echo "There are ${python_files_changed} modified Python files. Mutation testing can't proceed. Stopping this launcher"
+            echo -e "\n#######################################################################"
+            exit 1
+        fi
     fi
 done
 
