c8650486eb942747641d68b8f9480c3530fa53fb
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-12 17:07:36 -0300

    [MUTATION TESTING WITH COSMIC RAY] Additional changes to fix exit behavior

--- mutation_testing/launch_local_distributor_and_monitor.sh
+++ mutation_testing/launch_local_distributor_and_monitor.sh
@@ -14,12 +14,12 @@ while [[ "$python_files_changed" -lt 2 ]]; do
     if [[ -n "${distributor_pid}" ]]; then
         echo -e "\n\e[33mLocal distributor is already running with PID: ${distributor_pid}.\e[0m"
     else
-        source mutation_testing/Local_distributor/launch.sh &
+        bash mutation_testing/Local_distributor/launch.sh &
         distributor_pid=$!
         echo -e "\nLocal distributor PID: ${distributor_pid}"
     fi
 
-    source mutation_testing/check_status_periodically.sh "$1" --single-check &
+    bash mutation_testing/check_status_periodically.sh "$1" --single-check &
     monitor_pid=$!
     echo -e "\nMonitor PID: ${monitor_pid}"
 
@@ -35,6 +35,9 @@ while [[ "$python_files_changed" -lt 2 ]]; do
         echo -e "\n\n\e[31m#######################################################################"
         echo "Monitor detected no progress. Stopping the local distributor."
         python_files_changed=$(git st | grep -E 'modified:.*\.py' | wc -l)
+        if [[ "${python_files_changed}" -gt 0 ]]; then
+            echo "There are ${python_files_changed} modified Python files. Mutation testing can't proceed. Stopping this launcher"
+        fi
         echo "There are ${python_files_changed} modified Python files. Mutation testing can't proceed. Stopping this launcher"
         echo -e "#######################################################################\e[0m\n\n"
         distributor_pid=""
@@ -53,7 +56,7 @@ while [[ "$python_files_changed" -lt 2 ]]; do
     fi
 
     if [ "${monitor_rc}" -eq 2 ]; then
-        echo -e "\n\n\e[32m#######################################################################"
+        echo -e "\n\n\e[31m#######################################################################"
         echo "Monitor detected more than one modified Python file at a given time."
         echo -e "#######################################################################\e[0m\n\n"
         kill "${distributor_pid}" 2>/dev/null
@@ -62,7 +65,7 @@ while [[ "$python_files_changed" -lt 2 ]]; do
 done
 
 if [ "$python_files_changed" -lt 2 ]; then
-    echo -e "\n\n\e[32m#######################################################################"
+    echo -e "\n\n\e[31m#######################################################################"
     echo "Monitor detected more than one modified Python file at a given time."
     echo -e "#######################################################################\e[0m\n\n"
     exit 1
