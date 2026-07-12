18d4160b4b0735bae3a1e6e491ed8529d80adf43
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-12 18:38:45 -0300

    Sample scripts to test delegated execution and killing process

---
+++ child_script_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+    echo -e "\e[32mChild script 1 is running...\e[0m"
+    sleep 5
+done
---
+++ child_script_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+    echo -e "\e[31mChild script 2 is running...\e[0m"
+    sleep 1
+done
