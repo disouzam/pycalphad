6358d420599636b0b0fcfbf3583c792a6cdd3d6e
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-12 14:30:23 -0300

    Launcher script with automatic killing of local distributor of Cosmic Ray when it stops

--- mutation_testing/launch_local_distributor_and_monitor.sh
+++ mutation_testing/launch_local_distributor_and_monitor.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-bash mutation_testing/Local_distributor/launch.sh
-bash mutation_testing/check_status_periodically.sh "$1"
+cleanup() {
+    local distributor_pid="$1"
+    local monitor_pid="$2"
+    kill "${distributor_pid}" "${monitor_pid}" 2>/dev/null
+}
+
+while true; do
+    bash mutation_testing/Local_distributor/launch.sh &
+    distributor_pid=$!
+    echo "Local distributor PID: ${distributor_pid}"
+
+    bash mutation_testing/check_status_periodically.sh "$1" &
+    monitor_pid=$!
+    echo "Monitor PID: ${monitor_pid}"
+
+    trap 'cleanup "${distributor_pid}" "${monitor_pid}"' INT TERM EXIT
+
+    wait "${monitor_pid}"
+    monitor_rc=$?
+    echo "Monitor return code: ${monitor_rc}"
+
+    if [ "${monitor_rc}" -eq 1 ]; then
+        kill "${distributor_pid}" 2>/dev/null
+    fi
+done
+
