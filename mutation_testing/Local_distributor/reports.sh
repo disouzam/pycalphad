a4eb80ee79a36f99751fd71b8d40cbb4d70a813f
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-26 09:07:57 -0300

    Adjusted all paths and file names to run Cosmic Ray for local distributor and for  2 and 10 workers
    (cherry picked from commit ba5027b52dda0f0abce1d60b61ceffb9d46edfba)

--- mutation_testing/Local_distributor/reports.sh
+++ mutation_testing/Local_distributor/reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 directory="mutation_testing/Local_distributor"
-uv run cr-report ${directory}/cosmic-ray-for-local-distributor.sqlite
+uv run cr-report ${directory}/cosmic_ray.sqlite
 echo $(date)
