4aeb263340cfdeb8f69dbf1a4a5af8ea82c2b0ea
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-25 21:15:44 -0300

    Modifications in script list_files_with_git_status to behave recursively

--- tools/list_files_with_git_status.sh
+++ tools/list_files_with_git_status.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# References used:
+# - https://unix.stackexchange.com/questions/609287/bash-array-appending-element-issue/609293#609293
+# - https://stackoverflow.com/questions/51306244/list-files-with-git-status/70721980#70721980
+
 function custom_find_folders() {
   # find all folders in the current directory, excluding certain paths
-  result=$(find . -maxdepth 1 -type d -not -path '*/.git*' -not -path '*/_dev*' -not -path '*/.devcontainer*' -not -path '*/.venv*' -not -path '*/__pycache__*' -not -path '*/.mypy_cache*' -not -path '*/.pytest_cache*' -not -path '*/.vscode*' -not -path '*/.idea*' -not -path '*/.ipynb_checkpoints*' -not -path '*/*.egg-info*' -not -path '*/*build*' -not -path '*/*static*' -not -path '*/dist*' )
+  result=$(find "$1" -maxdepth 1 -type d -not -path '*/.git*' -not -path '*/_dev*' -not -path '*/.devcontainer*' -not -path '*/.venv*' -not -path '*/__pycache__*' -not -path '*/.mypy_cache*' -not -path '*/.pytest_cache*' -not -path '*/.vscode*' -not -path '*/.idea*' -not -path '*/.ipynb_checkpoints*' -not -path '*/*.egg-info*' -not -path '*/*build*' -not -path '*/*static*' -not -path '*/dist*' )
   echo "$result"
 }
 
-function list_files_with_git_status() {
+function list_files_in_a_single_folder_with_git_status() {
   for FILE in *
   do
     if [[ -f $FILE ]]
@@ -27,13 +31,23 @@ function list_files_with_git_status() {
   done
 }
 
-folders=$(custom_find_folders .)
+function walk_folders_recursive() {
+  local folder="$1"
+
+  echo -e "\nFolder: $folder"
+  pushd "$folder" > /dev/null || return
+  list_files_in_a_single_folder_with_git_status
+
+  while IFS= read -r subfolder; do
+    [[ "$subfolder" == "." ]] && continue
+    walk_folders_recursive "$subfolder"
+  done < <(custom_find_folders .)
+
+  popd > /dev/null || return
+}
+
+folders=$(custom_find_folders $1)
 
 for folder in $folders; do
-  echo -e "\nFolder: $folder"
-  pushd "$folder" > /dev/null || continue
-  list_files_with_git_status
-  subfolders=$(custom_find_folders .)
-  echo ${subfolders[@]}
-  popd > /dev/null || continue
+  walk_folders_recursive "$folder"
 done
